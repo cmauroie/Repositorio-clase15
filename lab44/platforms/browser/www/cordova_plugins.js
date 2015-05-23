c634adebd239192b43bb6f1c71698006cfd279c3
Carlos Mauricio Idárraga <ing-mauricio@outlook.com>
2015-05-23 07:45:59 -0500

    Clase 16 plugin Vibration

---
+++ lab44/platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/browser/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_browser",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "org.apache.cordova.vibration": "0.3.13"
+}
+// BOTTOM OF METADATA
+});
