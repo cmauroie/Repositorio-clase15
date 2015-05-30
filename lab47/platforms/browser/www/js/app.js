fd3e91bb06252c82083ac477daeb619583142293
Carlos Mauricio Idárraga <ing-mauricio@outlook.com>
2015-05-30 10:50:51 -0500

    Clase 16 - 17
    Ejemplos:
    - plugin device.
    - plugin camera.
    - plugin storage.

---
+++ lab47/platforms/browser/www/js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+var element = document.getElementById('deviceProperties');
+element.innerHTML = 'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+}
+
+function onDeviceReady_button(){
+	document.getElementById('deviceProperties2')
+	.innerHTML = 'Boton'+'<br />'+'Device Model: '    + device.model    + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
+}
